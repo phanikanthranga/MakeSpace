6c864b9b4826e3cb9457f4ac742c8020c6be910f
phanikanthranga <47543257+phanikanthranga@users.noreply.github.com>
2024-11-20 16:03:45 -0500

    Selenium Java code to order physical storage space on MakeSpace.com

--- MakeSpace.java
+++ MakeSpace.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 
-public class Alerts_Frames 
+public class MakeSpace 
 {
 	public static void main(String[] args) throws InterruptedException
 	{
