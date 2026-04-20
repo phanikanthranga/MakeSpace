9e177039aca17b14bd340182d23f4dc9807224c1
Phanikanth Ranga <47543257+phanikanthranga@users.noreply.github.com>
2026-04-20 12:32:25 -0400

    Java code to click on Calendar in any UI

---
+++ Calendar.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Calendar {
+
+public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver", "C://work//chromedriver.exe");
+ChromeDriver driver=new ChromeDriver();
+driver.get("https://www.calendar.com");
+
+driver.findElementByXPath(".//*[@id='travel_date']").click();
+
+
+while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("May"))
+{
+driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+}
+
+
+List<WebElement> dates= driver.findElements(By.className("day"));
+
+int count=driver.findElements(By.className("day")).size();
+
+for(int i=0;i<count;i++)
+{
+String text=driver.findElements(By.className("day")).get(i).getText();
+if(text.equalsIgnoreCase("21"))
+{
+driver.findElements(By.className("day")).get(i).click();
+break;
+}
+
+}
+}
+
+}
