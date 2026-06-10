8c96fde4068cc6625f829959e6de5b8419cb2a24
Phanikanth Ranga <47543257+phanikanthranga@users.noreply.github.com>
2026-06-09 20:19:55 -0400

    Java code to reverse a string

--- ReverseString.java
+++ ReverseString.java
@@ -3,13 +3,13 @@ public class ReverseString
     public static String stringRev(String str)
     {  
         char s[]=str.toCharArray();  
-        String reverse="";  
+        String reverseStr="";  
         int i=s.length-1;
         while(i>=0) 
         {  
-            reverse+=s[i];
+            reverseStr+=s[i];
             i--;
         }  
-        return reverse;  
+        return reverseStr;  
     }  
 }
