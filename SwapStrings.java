34cf1dd63975578e77ca72c9eeec7b486df6b519
Phanikanth Ranga <47543257+phanikanthranga@users.noreply.github.com>
2026-08-10 10:49:23 -0400

    Java code to Swap Strings without using temp

--- SwapStrings.java
+++ SwapStrings.java
@@ -2,8 +2,8 @@ public class SwapStrings
 {  
     public static void main(String args[]) 
     {  
-        String a = "First";  
-        String b = "Second String";  
+        String a = "First String";  
+        String b = "Second";  
         System.out.println("Before Swap: " + a + " " + b);  
         a = a + b;  
         b = a.substring(0, a.length() - b.length());  
